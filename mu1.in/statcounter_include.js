37a78d41af6f7bb4d41e6d378d2b3317796c74d2
Ashish Kumar <ashish.kumar@vdx.tv>
2025-06-11 05:26:44 +0530

    Initial commit of all files

---
+++ mu1.in/statcounter_include.js
@@ -0,0 +1,15 @@
+// Default Statcounter code for Motivational Umbrella
+// http://mu1.in
+var sc_project=13143088;
+var sc_invisible=1;
+var sc_security="5fd7c403";
+
+// Dynamically load the external Statcounter script
+(function() {
+    var sc = document.createElement('script');
+    sc.type = 'text/javascript';
+    sc.async = true;
+    sc.src = 'https://www.statcounter.com/counter/counter.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(sc, s);
+})();
